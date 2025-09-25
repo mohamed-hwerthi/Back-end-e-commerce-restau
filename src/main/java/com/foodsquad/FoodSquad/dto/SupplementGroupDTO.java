61ef01988df293ede781c26122efbffae41b4e1d
mohamed <hwerthimohamed25@gmail.com>
2025-09-25 14:35:54 +0100

    Implement logic to save produc supplemeents

---
+++ src/main/java/com/foodsquad/FoodSquad/dto/SupplementGroupDTO.java
@@ -0,0 +1,29 @@
+package com.foodsquad.FoodSquad.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.foodsquad.FoodSquad.model.entity.LocalizedString;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SupplementGroupDTO {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private UUID id;
+
+    @NotEmpty(message = "Group name cannot be blank")
+    private LocalizedString name;
+
+    private boolean obligatory;
+
+    private List<SupplementOptionDTO> supplementOptions  = new ArrayList<>();
+}
---
+++ src/main/java/com/foodsquad/FoodSquad/dto/SupplementOptionDTO.java
@@ -0,0 +1,29 @@
+package com.foodsquad.FoodSquad.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.foodsquad.FoodSquad.model.entity.LocalizedString;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SupplementOptionDTO {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private UUID id;
+
+    @NotEmpty(message = "Option name cannot be blank")
+    private LocalizedString name;
+
+    @Min(value = 0, message = "Price must be at least 0")
+    private BigDecimal price;
+
+}
